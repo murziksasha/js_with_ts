a836917220e921dc3cbcb910f57e65d1cdae0de1
Alexander Grygoriev <truedevelop@yahoo.com>
2023-04-03 07:01:25 +0300

    tasks for hours DONE

--- dist/head.js
+++ dist/head.js
@@ -1,2 +1,24 @@
 export function loggerMe() {
+    function getTimeFromMinutes(num) {
+        let resultHours = Math.floor(num / 60);
+        let resultMinutes = num % 60;
+        const result = `Это ${resultHours} часов и ${resultMinutes} минут`;
+        if (typeof num !== 'number' || !Number.isInteger(num) || num < 0) {
+            return "Ошибка, проверьте данные";
+        }
+        else if (resultHours === 0) {
+            return `Это ${resultHours} часов и ${resultMinutes} минут`;
+        }
+        else if (resultHours === 1) {
+            return `Это ${resultHours} час и ${resultMinutes} минут`;
+        }
+        else if (resultHours > 1 && resultHours <= 4) {
+            return `Это ${resultHours} часа и ${resultMinutes} минут`;
+        }
+        else if (resultHours >= 5 && resultHours <= 10) {
+            return `Это ${resultHours} часов и ${resultMinutes} минут`;
+        }
+        return result;
+    }
+    console.log(getTimeFromMinutes(50));
 }
--- src/head.ts
+++ src/head.ts
@@ -1,6 +1,30 @@
 export function loggerMe(): void {
 
+  function getTimeFromMinutes(num: number): string {
 
+    let resultHours: number =  Math.floor(num / 60);
+    let resultMinutes: number = num % 60;
+
+    const result =  `Это ${resultHours} часов и ${resultMinutes} минут`
+
+    if(typeof num !== 'number' || !Number.isInteger(num) || num < 0){
+      return "Ошибка, проверьте данные"
+    } else if (resultHours === 0){
+      return `Это ${resultHours} часов и ${resultMinutes} минут`;
+  } else if(resultHours === 1){
+      return `Это ${resultHours} час и ${resultMinutes} минут`;
+  }else if (resultHours > 1 && resultHours <= 4) {
+      return `Это ${resultHours} часа и ${resultMinutes} минут`;
+  } else if (resultHours >= 5 && resultHours <= 10) {
+      return `Это ${resultHours} часов и ${resultMinutes} минут`;
+  }
+
+    return result;
+
+
+  }
+
+  console.log(getTimeFromMinutes(50));
 
 
 
