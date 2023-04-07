c7f8460c9207213343aff8805b3696ba7e60250a
Alexander Grygoriev <truedevelop@yahoo.com>
2023-04-07 07:00:45 +0300

    cool tasks from petrech

---
+++ src/mars_proj/js/script.ts
@@ -0,0 +1,38 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const advImages = document.querySelectorAll('div.promo__adv img');
+const genreDiv = document.querySelector('div.promo__genre') as HTMLDivElement;
+const divPromo = document.querySelector('div.promo__bg') as HTMLDivElement;
+
+advImages.forEach(item => {
+    item.remove();
+});
+
+genreDiv.textContent = 'драма';
+
+divPromo ? divPromo.style.cssText = 'background: url(./img/bg.jpg) center center/cover no-repeat;' : null;
+
