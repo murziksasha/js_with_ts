f691b7107cb4630d17bfd40ebb8e14fab6e93878
Alexander Grygoriev <truedevelop@yahoo.com>
2023-04-13 21:53:49 +0300

    deligation of listener learning

--- dist/head.js
+++ dist/head.js
@@ -1,12 +1,16 @@
 export function loggerMe() {
     const btns = document.querySelectorAll('button');
+    const parent = document.querySelector('#first');
     // console.log(btns[0].classList.item(0));
-    btns[0].addEventListener('click', () => {
-        if (btns[3].classList.contains('red')) {
-            btns[3].classList.remove('red');
-        }
-        else {
-            btns[3].classList.add('red');
+    parent.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && target.matches('[data-current = "1"]')) {
+            if (btns[3].classList.contains('red')) {
+                btns[3].classList.remove('red');
+            }
+            else {
+                btns[3].classList.add('red');
+            }
         }
     });
 }
--- src/head.ts
+++ src/head.ts
@@ -1,16 +1,22 @@
 export function loggerMe(): void {
 
 const btns = document.querySelectorAll('button');
+const parent = document.querySelector('#first') as HTMLDivElement;
 
 // console.log(btns[0].classList.item(0));
 
-btns[0].addEventListener('click', ()=>{
+parent.addEventListener('click', (e)=>{
 
-  if(btns[3].classList.contains('red')){
-    btns[3].classList.remove('red');
-  } else {
-    btns[3].classList.add('red');
+  const target = e.target as HTMLButtonElement;
+
+  if(target && target.matches('[data-current = "1"]')){    
+    if(btns[3].classList.contains('red')){
+      btns[3].classList.remove('red');
+    } else {
+      btns[3].classList.add('red');
+    }
   }
+
 })
 
 
