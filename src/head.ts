1ca572aa9cccd5b8a528a9435406bd4935c4a22d
Alexander Grygoriev <truedevelop@yahoo.com>
2023-04-20 07:33:01 +0300

    TS in Promise

--- src/head.ts
+++ src/head.ts
@@ -1,7 +1,45 @@
 export function loggerMe(): void {
 
+  console.log('loading...')
 
+  interface Product {
+    name: string;
+    price: number;
+    status: string;
+    modify: boolean;
+  }
 
+const req = new Promise<Product>(function(resolve, reject){
+
+  setTimeout(()=>{
+    console.log('Preparing data...');
+    const product: Product = {
+      name: 'TV',
+      price: 2000,
+      status: '',
+      modify: false,
+    }
+    resolve(product);
+
+  }, 2000)
+
+});
+
+req.then((data: Product)=>{
+  return new Promise<Product>((res, rej) => {
+    setTimeout(()=> {
+      data.status = 'order';
+      res(data);
+    }, 2000)
+  });
+  
+}).then((data: Product) => {
+    data.modify = true;
+    return data;
+  }).then((data:Product)=>{
+    
+    console.log(data);
+})
 
 
 
